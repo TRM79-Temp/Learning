b33891f4d1dd539929a8f0bd5744a1b527b89ff1
TRM79-Temp <roman_tarasiuk@ukr.net>
2019-11-30 13:35:25 +0200

    Added 'Add new project' functionality; Some refactoring.

--- ASP.NET Core MVC 2/scripts.js
+++ ASP.NET Core MVC 2/scripts.js
@@ -3,6 +3,7 @@
 //     active: true,
 //     apiUrl: 'https://api.github.com/repos/TRM79-Temp/ASP-PartyInvites-2.0/commits',
 //     commits: localStorage.party_Invites_2_0
+//     text: ''
 // }])
 
 console.log('Github API Helper - v.0.28');
@@ -99,7 +100,6 @@ window.GithubApiHelper = function () {
 
     this.saveCommits = function (name, commitsApiUrl, commits, text) {
         // setCookie('gitCommits', txt.replace(/;/g, '$$$'), 1000);
-        // localStorage.setItem('party_Invites_2_0', txt);
         var found = false;
         for (var i = 0; i < this.projects.length; i++) {
             if (this.projects[i].name == name) {
@@ -119,7 +119,7 @@ window.GithubApiHelper = function () {
             });
         }
 
-        localStorage.setItem('projects', JSON.stringify(this.projects));
+        this.saveChanges();
     };
 
     this.getActiveProject = function () {
@@ -130,17 +130,51 @@ window.GithubApiHelper = function () {
             }
         }
 
-        return null;
-    }
+        // No active projects found.
+        if (this.projects.length > 0) {
+            return this.projects[this.projects.length - 1];
+        }
+        else {
+            return null;
+        }
+    };
 
-    this.changeActiveProject = function (project) {
+    this.setActiveProject = function (projectName) {
+        for (var i = 0; i < this.projects.length; i++) {
+            if (this.projects[i].name == projectName) {
+                this.projects[i].active = true;
+            }
+            else {
+                this.projects[i].active = false;
+            }
+        }
+    };
 
+    this.containsProject = function (projectName) {
+        for (var i = 0; i < this.projects.length; i++) {
+            if (this.projects[i].name == projectName) {
+                return true;
+            }
+        }
+
+        return false;
+    };
+
+    this.saveChanges = function () {
+        localStorage.setItem('projects', JSON.stringify(this.projects));
     }
 }
 
-//
+/*
+ *  Processing data.
+ */
 
 window.onload = function () {
+
+    var githubApiHelper = new window.GithubApiHelper();
+
+    // Helper functions.
+
     function download(data, filename, type) {
         var file = new Blob([data], {type: type});
         if (window.navigator.msSaveOrOpenBlob) // IE10+
@@ -187,9 +221,45 @@ window.onload = function () {
         localStorage.setItem('projects', notesText);
     }
 
-    //
+    function showProject (activeProject) {
+        document.getElementById('commitsApiUrl').value = activeProject.apiUrl;
+        document.getElementById('commits').value = activeProject.commits;
+        document.getElementById('textEdit').value = activeProject.text;
 
-    var githubApiHelper = new window.GithubApiHelper();
+        //
+
+        if (!githubApiHelper.editingMode) {
+            document.getElementById('text').innerHTML = activeProject.text;
+        }
+
+        //
+
+        var commits = JSON.parse(activeProject.commits);
+        var commitsSpan = document.getElementsByTagName('span');
+
+        for (var i = 0; i < commitsSpan.length; i++) {
+            if (commitsSpan[i].hasAttribute('data-listing')) {
+                var listingAttr = commitsSpan[i].getAttribute('data-listing');
+                if (typeof(listingAttr) == 'undefined' || listingAttr == null || listingAttr == '') {
+                    continue;
+                }
+
+                var commitIndex = commits.findIndex(el => el.message == listingAttr);
+                if (commitIndex >= 0) {
+                    commitsSpan[i].innerHTML =
+                        '<a href="' + commits[commitIndex].blob_url + '">' + listingAttr + '</a>';
+
+                    var previousVersionIndex = commits.findIndex(el => el.fileName == commits[commitIndex].fileName);
+                    if (previousVersionIndex < commitIndex) {
+                        commitsSpan[i].innerHTML +=
+                            ' (<a href="' + commits[commitIndex].url + '">Δ</a>)'
+                    }
+                }
+            }
+        }
+    }
+
+    // Event handlers.
 
     document.getElementById('btnDownload').onclick = async function() {
         console.log('Download');
@@ -227,10 +297,6 @@ window.onload = function () {
 
     document.getElementById('btnSave').onclick = function() {
         console.log('Save');
-        // console.log(document.getElementById('projects').value);
-        // console.log(document.getElementById('commitsApiUrl').value);
-        // console.log(document.getElementById('commits').value);
-        // console.log(document.getElementById('textEdit').value);
         githubApiHelper.saveCommits(
             document.getElementById('projects').value,
             document.getElementById('commitsApiUrl').value,
@@ -245,7 +311,11 @@ window.onload = function () {
     }
 
     document.getElementById('projects').onchange = function() {
-        console.log(this.value);
+        var projectName = this.value;
+        githubApiHelper.setActiveProject(projectName);
+        githubApiHelper.saveChanges();
+        var project = githubApiHelper.getActiveProject();
+        showProject(project);
     }
 
     document.getElementById('editingMode').onchange = function() {
@@ -261,14 +331,40 @@ window.onload = function () {
         importNotes(event);
     }
 
-    // -------------------------------------------------
+    document.getElementById('btnAddNew').onclick = function() {
+        var projectName = document.getElementById('newProject').value;
+        if (projectName == '') {
+            alert('Enter the project name.');
+            return;
+        }
+
+        if (githubApiHelper.containsProject(projectName)) {
+            alert('A project with the name \'' + projectName + '\' already exists.');
+            return;
+        }
+
+        githubApiHelper.saveCommits(projectName, '', '', '');
+        githubApiHelper.setActiveProject(projectName);
+        githubApiHelper.saveChanges();
+        alert('Project added. Please reload page.');
+    }
+
+    // Getting projects and displaying an active.
+
+    var activeProject = githubApiHelper.getActiveProject();
+    var activeProjectIndex = 0;
 
     var selectProject = '';
     for (var i = 0; i < githubApiHelper.projects.length; i++) {
         selectProject += '<option>' + githubApiHelper.projects[i].name + '</option>';
+        if (githubApiHelper.projects[i].name == activeProject.name) {
+            activeProjectIndex = i;
+        }
     }
 
-    this.document.getElementById('projects').innerHTML = selectProject;
+    var projectSelect = this.document.getElementById('projects');
+    projectSelect.innerHTML = selectProject;
+    projectSelect.selectedIndex = activeProjectIndex;
 
     //
 
@@ -276,40 +372,5 @@ window.onload = function () {
 
     //
 
-    var activeProject = githubApiHelper.getActiveProject();
-    document.getElementById('commitsApiUrl').value = activeProject.apiUrl;
-    document.getElementById('commits').value = activeProject.commits;
-    document.getElementById('textEdit').value = activeProject.text;
-
-    //
-
-    if (!githubApiHelper.editingMode) {
-        document.getElementById('text').innerHTML = activeProject.text;
-    }
-
-    //
-
-    var commits = JSON.parse(activeProject.commits);
-    var commitsSpan = document.getElementsByTagName('span');
-
-    for (var i = 0; i < commitsSpan.length; i++) {
-        if (commitsSpan[i].hasAttribute('data-listing')) {
-            var listingAttr = commitsSpan[i].getAttribute('data-listing');
-            if (typeof(listingAttr) == 'undefined' || listingAttr == null || listingAttr == '') {
-                continue;
-            }
-
-            var commitIndex = commits.findIndex(el => el.message == listingAttr);
-            if (commitIndex >= 0) {
-                commitsSpan[i].innerHTML =
-                    '<a href="' + commits[commitIndex].blob_url + '">' + listingAttr + '</a>';
-
-                var previousVersionIndex = commits.findIndex(el => el.fileName == commits[commitIndex].fileName);
-                if (previousVersionIndex < commitIndex) {
-                    commitsSpan[i].innerHTML +=
-                        ' (<a href="' + commits[commitIndex].url + '">Δ</a>)'
-                }
-            }
-        }
-    }
+    showProject(activeProject);
 }
